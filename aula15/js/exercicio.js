8c23daa9fd450c1314a85a6f4e4a0827566c6940
andre <andd68@hotmail.com>
2021-07-09 12:36:24 -0300

    numbers and math

---
+++ aula15/js/exercicio.js
@@ -0,0 +1,12 @@
+const numero = Number(prompt('Digite um número:')); //prompt sempre retorna string!
+const numeroTitulo = window.document.getElementById('numero-titulo');
+const texto = window.document.getElementById('texto');
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = `<p>Seu número -2 é: ${numero - 2}</p>`;
+texto.innerHTML += `<p>Seu número +2 é: ${numero + 2}</p>`;
+texto.innerHTML += `<p>Raíz quadrada: ${numero**(1/2)}</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Arredondado para baixo: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Arredondado para cima: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
